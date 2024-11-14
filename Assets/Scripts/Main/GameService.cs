95bc59ac1de9cdd3b7dbc0eb7a23c8c6fac58618
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-14 10:43:04 +0530

    updated sound service with locator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using ServiceLocator.Utilities;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; }
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+    public SoundService soundService { get; private set; }
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
     }
 
     private void Update()
