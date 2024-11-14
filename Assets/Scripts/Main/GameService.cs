3a91b15b1e3e2a6ac36985675e84666813b2a348
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-14 11:08:59 +0530

    updated map service with locator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -5,25 +5,33 @@ using ServiceLocator.Utilities;
 using ServiceLocator.Player;
 using ServiceLocator.Sound;
 using ServiceLocator.UI;
+using ServiceLocator.Map;
 
 public class GameService : GenericMonoSingleton<GameService>
 {
     public PlayerService playerService { get; private set; }
     public SoundService soundService { get; private set; }
+    public MapService mapService { get; private set; }
 
     [SerializeField] private UIService uiService;
     public UIService UIService => uiService;
 
+    //Player
     [SerializeField] private PlayerScriptableObject playerScriptableObject;
 
+    //Sound
     [SerializeField] private SoundScriptableObject soundScriptableObject;
     [SerializeField] private AudioSource audioEffects;
     [SerializeField] private AudioSource backgroundMusic;
+
+    //Map
+    [SerializeField] private MapScriptableObject mapScriptableObject;
 
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
         soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        mapService = new MapService(mapScriptableObject);
     }
 
     private void Update()
