2a32455bd79a530e52737f0f85be26ff7965d82b
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-14 10:20:29 +0530

    created Service Locator(GameService)

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -8,10 +8,9 @@ using ServiceLocator.Sound;
 
 namespace ServiceLocator.Player
 {
-    public class PlayerService : GenericMonoSingleton<PlayerService>
+    public class PlayerService
     {
-        [SerializeField] public PlayerScriptableObject playerScriptableObject;
-
+        private PlayerScriptableObject playerScriptableObject;
         private ProjectilePool projectilePool;
 
         private List<MonkeyController> activeMonkeys;
@@ -20,8 +19,9 @@ namespace ServiceLocator.Player
         private int money;
         public int Money => money;
 
-        private void Start()
+        public PlayerService(PlayerScriptableObject playerScriptableObject)
         {
+            this.playerScriptableObject = playerScriptableObject;
             projectilePool = new ProjectilePool(playerScriptableObject.ProjectilePrefab, playerScriptableObject.ProjectileScriptableObjects);
             InitializeVariables();
         }
